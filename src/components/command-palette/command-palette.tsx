d9a7dca276615f4b877232388a2a7d43c881f0a3
Kenneth Wußmann <kenneth@wussmann.net>
2024-11-20 19:49:36 +0100

    Add image selection to command palette (#30)

--- src/components/command-palette/command-palette.tsx
+++ src/components/command-palette/command-palette.tsx
@@ -4,12 +4,16 @@ import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, C
 import { Command, getCategorizedCommands } from "@/lib/commands";
 import { Shortcut } from "@/lib/settings";
 import { ShortcutText } from "../common/shortcut-text";
+import { useImages } from "@/hooks/use-images";
+import { useImageNavigation } from "@/hooks/provider/image-navigation-provider";
 
 const hiddenCommands: Shortcut[] = ["openCommandPalette"];
 
 export const CommandPalette = () => {
   const [isOpen, setOpen] = useState(false);
   const commands = getCategorizedCommands();
+  const { allImages } = useImages();
+  const { selectImage } = useImageNavigation()
   useShortcut("openCommandPalette", () => {
     setOpen((current) => !current);
   })
@@ -19,12 +23,17 @@ export const CommandPalette = () => {
     command.execute();
   }
 
+  const onSelectImage = (filename: string) => {
+    setOpen(false);
+    selectImage(filename);
+  }
+
   return (
     <CommandDialog open={isOpen} onOpenChange={setOpen}>
-      <CommandInput placeholder="Type a command or search..." />
+      <CommandInput placeholder="Type a command or search for image..." />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
-        {Object.entries(commands).map(([category, commands]) => (
+        {Object.entries(commands).map(([category, commands]) => commands.length > 0 ? (
           <CommandGroup key={category} heading={category}>
             {commands.filter(
               command => !hiddenCommands.includes(command.shortcut)
@@ -36,7 +45,16 @@ export const CommandPalette = () => {
               </CommandItem>
             ))}
           </CommandGroup>
-        ))}
+        ) : <></>)}
+        {allImages.length > 0 && (
+          <CommandGroup heading="Images">
+            {allImages.map(image => (
+              <CommandItem key={image.id} onSelect={() => onSelectImage(image.filename)}>
+                <span>{image.filename}</span>
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        )}
       </CommandList>
     </CommandDialog>
   )
