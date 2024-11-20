f65bd3a840a9a7f698eefe3180549a4bb22f6c9b
Kenneth Wußmann <kenneth@wussmann.net>
2024-11-20 16:04:02 +0100

    Allow navigation through parts with arrow keys

--- src/components/caption/caption-view.tsx
+++ src/components/caption/caption-view.tsx
@@ -2,26 +2,36 @@ import { CaptionInput } from "./components/caption-input";
 import { CaptionList } from "./components/caption-list";
 import { ImageViewer } from "../common/image-viewer";
 import { useImageNavigation } from "@/hooks/provider/image-navigation-provider";
+import { ArrowKeyNavigationProvider } from "../common/arrow-key-navigation-provider";
+import { useCaptionEditor } from "./caption-editor-provider";
 
 export const CaptionView = () => {
+  const { enterEditMode, parts } = useCaptionEditor()
   const { currentImage } = useImageNavigation();
+
+  const editPart = (index: number) => {
+    enterEditMode(parts[index])
+  }
+
   if (!currentImage) {
     return null;
   }
   return (
     <div className="relative flex flex-col h-full gap-4">
-      <div className="flex flex-grow">
-        <div className="flex-grow w-full h-full flex items-center justify-center overflow-hidden">
-          <ImageViewer image={currentImage} />
-        </div>
+      <ArrowKeyNavigationProvider onSubmit={editPart}>
+        <div className="flex flex-grow">
+          <div className="flex-grow w-full h-full flex items-center justify-center overflow-hidden">
+            <ImageViewer image={currentImage} />
+          </div>
 
-        <div className="w-1/3 h-full flex flex-col">
-          <div className="flex-grow overflow-y-auto">
-            <CaptionList />
+          <div className="w-1/3 h-full flex flex-col">
+            <div className="flex-grow overflow-y-auto">
+              <CaptionList />
+            </div>
           </div>
         </div>
-      </div>
-      <CaptionInput />
+        <CaptionInput />
+      </ArrowKeyNavigationProvider>
     </div>
   );
 };
